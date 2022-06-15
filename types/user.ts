275e72c3c16bce84843b2b5503a6954c35fdb8bd
hakuu1251 <hakuu1251@gmail.com>
2022-06-15 13:32:03 +0300

    Updated IUser

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,7 @@
-import { SchemaTimestampsConfig } from 'mongoose'
 export interface IUser {
 	_id: string
 	username: string
 	email: string
 	type: string
 	isRemoved: boolean
-	timestamps: SchemaTimestampsConfig
 }
